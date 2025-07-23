1552ed077ebc3ba68cf76dcc9b3278b33430cf13
Aabbaaii13 <photoman894@gmail.com>
2025-07-23 23:50:49 +0500

    Update whitelist.lua

--- whitelist.lua
+++ whitelist.lua
@@ -1,6 +1,6 @@
 local whitelist = {
     ["g52glw6gwwdof0xxciixyjzz6yll6fxr043fnfv1q08c92g441"] = true,
-    ["hwid_2"] = true,
+    ["nyiczsmgy23is17aph6x117c9f93zctwvqeg3wjgxl6zgrmt5w"] = true,
 }
 
 if not isfile("hwid.key") then
