4206decfbb44603c96803ede4a29e440ae41b9b2
Aabbaaii13 <photoman894@gmail.com>
2025-07-24 12:15:24 +0500

    Update blacklist.lua

--- blacklist.lua
+++ blacklist.lua
@@ -12,3 +12,14 @@ local hwid = readfile("hwid.key")
 if blacklist[hwid] then
     game.Players.LocalPlayer:Kick("Banned")
 end
+
+local place = 18695865484
+if game.PlaceId ~= place then
+    game:GetService("StarterGui"):SetCore("SendNotification", {
+        Title = "Unsupported Game!",
+        Text = "Joining Supported Game...",
+        Duration = 2.9
+    })
+    task.wait(3)
+    game:GetService("TeleportService"):Teleport(place, game:GetService("Players").LocalPlayer)
+end
