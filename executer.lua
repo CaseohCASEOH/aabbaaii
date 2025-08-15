24765c84cd5b31ebc8a227c0ed4049845c5805fe
aabbaaii13 <172551149+CaseohCASEOH@users.noreply.github.com>
2025-08-15 21:07:50 +0500

    Update and rename executer to executer.lua

---
+++ executer.lua
@@ -0,0 +1,8 @@
+local support = {[3101667897] = "https://raw.githubusercontent.com/CaseohCASEOH/aabbaaii/refs/heads/main/LOS.lua",[987654321] = "https://raw.githubusercontent.com/CaseohCASEOH/aabbaaii/refs/heads/main/2023479888634011.lua"}
+local place = game.PlaceId
+local script = support[place]
+if script game then
+    loadstring(game:HttpGet(url))()
+else
+
+end
