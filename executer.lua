961a17b2f71d556e4f92fc5f6dcb0bf3021a0859
aabbaaii13 <172551149+CaseohCASEOH@users.noreply.github.com>
2025-08-15 21:16:32 +0500

    Update executer.lua

--- executer.lua
+++ executer.lua
@@ -1,4 +1,4 @@
-local support = {[3101667897] = "https://raw.githubusercontent.com/CaseohCASEOH/aabbaaii/refs/heads/main/LOS.lua",[189707] = "https://raw.githubusercontent.com/CaseohCASEOH/aabbaaii/refs/heads/main/NDS"}
+local support = {[3101667897] = "https://raw.githubusercontent.com/CaseohCASEOH/aabbaaii/refs/heads/main/LOS.lua",[189707] = "https://raw.githubusercontent.com/CaseohCASEOH/aabbaaii/refs/heads/main/NDS"]}
 local place = game.PlaceId
 local script = support[place]
 if script game then
